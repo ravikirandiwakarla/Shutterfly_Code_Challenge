6a22c1a4eb0a30904955dccc3f890756ba93c159
rdiwakarla <rdiwakarla@shutterfly.com>
2021-11-25 11:01:55 -0700

    fixing the path for one of the test

--- tests/test_file_util.py
+++ tests/test_file_util.py
@@ -6,10 +6,10 @@ class TestFileUtil(unittest.TestCase):
     # Test method to test the read_json_file function
     def test_read_json_file(self):
         # test case 1
-        self.assertIsNotNone(read_json_file('test_files/test_good_file.txt'))
+        self.assertIsNotNone(read_json_file('tests/test_files/test_good_file.txt'))
 
         # test case 2
         self.assertRaises(FileNotFoundError, read_json_file, 'test_no_such_file.txt')
 
         # test case 3
-        self.assertRaises(json.decoder.JSONDecodeError, read_json_file, 'test_files/test_bad_file.txt')
+        self.assertRaises(json.decoder.JSONDecodeError, read_json_file, 'tests/test_files/test_bad_file.txt')
